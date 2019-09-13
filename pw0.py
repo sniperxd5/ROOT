eb3fe84d1c7cf2ebbdbb9a6671226791b1f1e9dd
sniperxd5 <55256516+sniperxd5@users.noreply.github.com>
2019-09-13 03:27:42 +0100

    Create pw0.py

---
+++ pw0.py
@@ -0,0 +1,142 @@
+company-ansible-0.7.1/0000755000000000000000000000000013536557505013314 5ustar  rootrootcompany-ansible-0.7.1/company-ansible-keywords.el0000644000000000000000000052430413536557505020574 0ustar  rootroot;;; company-ansible-keywords.el --- A company back-end for ansible
+
+;; Copyright (c) 2015,2016 Krzysztof Magosa
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be included in all
+;; copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
+;;; Commentary:
+;; Generated automatically.
+;; Don't modify this file manually.
+
+;;; Code:
+
+(defconst company-ansible-keywords
+  '(
+    "%"
+    "*regex*"
+    "-"
+    "0"
+    "1"
+    "1.0"
+    "10"
+    "100GE"
+    "100ge"
+    "1024"
+    "10G"
+    "10GE"
+    "10ge"
+    "112"
+    "11G"
+    "12"
+    "16"
+    "2"
+    "2000"
+    "2000SP1"
+    "2005"
+    "2008"
+    "2008R2"
+    "2012"
+    "2014"
+    "24"
+    "25ge"
+    "2c"
+    "3"
+    "30"
+    "32"
+    "3XL"
+    "3des"
+    "3des168"
+    "4"
+    "40GE"
+    "40ge"
+    "443"
+    "4XL"
+    "4x10ge"
+    "5"
+    "5.1"
+    "5.6"
+    "5.7"
+    "512emulation"
+    "56"
+    "56k"
+    "5XL"
+    "6"
+    "6to4"
+    "7"
+    "70"
+    "8"
+    "80"
+    "802.1p"
+    "802.3ad"
+    "9"
+    "9.5"
+    "9.6"
+    "A"
+    "A-B"
+    "A6"
+    "AAAA"
+    "ACCEPT"
+    "ACTIVE"
+    "ADNS"
+    "ADNS_TCP"
+    "ADVANCED"
+    "AF11"
+    "AF12"
+    "AF13"
+    "AF21"
+    "AF22"
+    "AF23"
+    "AF31"
+    "AF32"
+    "AF33"
+    "AF41"
+    "AF42"
+    "AF43"
+    "ALIAS"
+    "ALWAYS"
+    "AMD_OPTERON"
+    "ANAME"
+    "ANY"
+    "Accounting"
+    "AddressBook"
+    "Admin"
+    "Aligned"
+    "Amazon"
+    "AppendData"
+    "Application"
+    "AuthRoot"
+    "Authentication"
+    "Authorization"
+    "Automap"
+    "Automatic"
+    "Azure"
+    "B"
+    "B-A"
+    "BANDWIDTH"
+    "BFD"
+    "BGP"
+    "BOF"
+    "BareMetal"
+    "Basic"
+    "BlobStorage"
+    "BranchCache"
+    "CAA"
+    "CERT_NONE"
+    "CERT_OPTIONAL"
+    "CERT_REQUIRED"
